29952320575ccdce99efaef645ab22d698e4259b
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-04 18:43:10 -0700

    created title display functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+void show_title();
 void welcome(int name);
+void show_stats(const char *planet, int credits, int fuel);
 
 int main() {
   // Todo: replace with string
   std::cout << "Enter number ";
   int name{};
   std::cin >> name;
+
+  const char *planet{"Earth"};
+  int credits{1000};
+  int fuel{50};
+
+  show_title();
   welcome(name);
-  return 0;
+  show_stats(planet, credits, fuel);
 }
 
-void welcome(int name) { std::cout << "Welcome " << name; }
+void show_title() {
+  std::cout << "\n----------------------"
+               "\nSpace Adventure Trader"
+               "\n----------------------";
+}
+
+void welcome(int name) { std::cout << "\nWelcome " << name; }
+
+void show_stats(const char *planet, int credits, int fuel) {
+  std::cout << "\n---------------------------------";
+  std::cout << "\nCurrent Planet: " << planet;
+  std::cout << "\nCredits: " << credits;
+  std::cout << "\nFuel: " << fuel << "/100";
+  std::cout << "\n---------------------------------";
+}
