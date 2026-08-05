4ea267a9ecc64880813cd288e4b115da5ed93ea7
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-04 18:30:58 -0700

    add welcome function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+void welcome(int name);
 
 int main() {
+  // Todo: replace with string
   std::cout << "Enter number ";
   int name{};
   std::cin >> name;
-  std::cout << "Hello " << name;
+  welcome(name);
   return 0;
 }
+
+void welcome(int name) { std::cout << "Welcome " << name; }
