153efa6f135947647b7c9c1640a5432167a06aac
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-09 22:54:19 -0700

    improve display

--- src/display.cpp
+++ src/display.cpp
@@ -1,19 +1,31 @@
 #include "../include/display.h"
+#include <iomanip>
 #include <iostream>
 #include <string>
 
+void show_line(bool double_line) {
+  if (double_line) {
+    std::cout << "\n==================================";
+  } else {
+    std::cout << "\n----------------------------------";
+  }
+}
+
 void show_title() {
-  std::cout << "\n----------------------"
-               "\nSpace Adventure Trader"
-               "\n----------------------";
+  show_line(true);
+  std::cout << std::left;
+  std::cout << std::setw(6) << "\n" << "Space Adventure Trader";
+  show_line(true);
 }
 
 void welcome(std::string name) { std::cout << "\nWelcome " << name; }
 
 void show_stats(const char *planet, int credits, int fuel) {
-  std::cout << "\n---------------------------------";
-  std::cout << "\nCurrent Planet: " << planet;
-  std::cout << "\nCredits: " << credits;
-  std::cout << "\nFuel: " << fuel << "/100";
-  std::cout << "\n---------------------------------";
+  int width{16};
+  std::cout << std::left;
+  show_line(false);
+  std::cout << std::setw(width) << "\nCurrent Planet" << ": " << planet;
+  std::cout << std::setw(width) << "\nCredits" << ": " << credits;
+  std::cout << std::setw(width) << "\nFuel" << ": " << fuel << "/100";
+  show_line(false);
 }
