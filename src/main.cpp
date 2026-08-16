f17a6c52ab0396f52315fcc314ebaf14b2c19253
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-15 20:19:00 -0700

    add gameplay choices (for now does nothing)

--- src/main.cpp
+++ src/main.cpp
@@ -15,4 +15,42 @@ int main() {
   showTitle();
   welcome(name);
   showStats(startPlanet, startCredits, startFuel);
+
+  bool playing{true};
+
+  while (playing) {
+    std::cout << "\n"
+              << "\nWhat do you want to do?"
+              << "\n1. Travel"
+              << "\n2. Buy"
+              << "\n3. Sell"
+              << "\n4. Profit Report"
+              << "\n0. Quit"
+              << "\n"
+              << "\nEnter your choice: ";
+
+    int choice{0};
+    std::cin >> choice;
+    switch (choice) {
+    case 1:
+      std::cout << "\nTravelling";
+      break;
+    case 2:
+      std::cout << "\nBuying";
+      break;
+    case 3:
+      std::cout << "\nSelling";
+      break;
+    case 4:
+      std::cout << "\nReporting";
+      break;
+    case 0:
+      std::cout << "\nGoodbye " << name << "!";
+      playing = false;
+      break;
+    default:
+      std::cout << "\nInvalid choice!";
+      break;
+    }
+  }
 }
